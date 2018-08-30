12bb5409333d75e6c6f61f92c14cc11457148efe
Leochrisis <leonardo.chfc@gmail.com>
2018-08-30 01:39:01 -0300

    Build user controller

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -3,13 +3,41 @@
 const User = use('App/Models/User')
 
 class UserController {
-  async create ({ request }) {
+  async index () {
+    const users = User.all()
+
+    return users
+  }
+
+  async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
 
     const user = await User.create(data)
 
     return user
   }
+
+  async show ({ params, response }) {
+    const user = await User.findOrFail(params.id)
+
+    if (!user) {
+      return response.status(404).json({ message: 'User not found!' })
+    }
+
+    return user
+  }
+
+  async update ({ params, request, response }) {
+    const user = await User.findOrFail(params.id)
+
+    const data = request.only(['username', 'email', 'password'])
+
+    user.merge(data)
+    await user.save()
+    return user
+  }
+
+  async destroy ({ params, auth, response }) {}
 }
 
 module.exports = UserController
