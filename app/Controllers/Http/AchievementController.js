c4119fd0a6daa54687ac6c5135b52088985f8264
Leochrisis <leonardo.chfc@gmail.com>
2018-09-16 15:31:52 -0300

    Remove unused comments

--- app/Controllers/Http/AchievementController.js
+++ app/Controllers/Http/AchievementController.js
@@ -2,24 +2,13 @@
 
 const Achievement = use('App/Models/Achievement')
 
-/**
- * Resourceful controller for interacting with achievements
- */
 class AchievementController {
-  /**
-   * Show a list of all achievements.
-   * GET achievements
-   */
   async index () {
     const achievements = Achievement.all()
 
     return achievements
   }
 
-  /**
-   * Create/save a new achievement.
-   * POST achievements
-   */
   async store ({ request, response }) {
     const data = request.only(['name', 'cnpj', 'phone', 'adress'])
 
@@ -28,10 +17,6 @@ class AchievementController {
     return achievement
   }
 
-  /**
-   * Display a single achievement.
-   * GET achievements/:id
-   */
   async show ({ params, response }) {
     const achievement = await Achievement.findOrFail(params.id)
 
@@ -42,10 +27,6 @@ class AchievementController {
     return achievement
   }
 
-  /**
-   * Update achievement details.
-   * PUT or PATCH achievements/:id
-   */
   async update ({ params, request, response }) {
     const achievement = await Achievement.findOrFail(params.id)
 
@@ -56,10 +37,6 @@ class AchievementController {
     return achievement
   }
 
-  /**
-   * Delete a achievement with id.
-   * DELETE achievements/:id
-   */
   async destroy ({ params }) {
     const achievement = await Achievement.findOrFail(params.id)
 
