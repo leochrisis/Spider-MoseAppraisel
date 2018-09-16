eb2b99e25b188bf0341ae07bb668073de7794fb1
Leochrisis <leonardo.chfc@gmail.com>
2018-09-16 15:31:00 -0300

    Add controller of achievement, user, profile and team relation to project

---
+++ app/Controllers/Http/ProfileAchievementsTeamController.js
@@ -0,0 +1,57 @@
+'use strict'
+
+/**
+ * Resourceful controller for interacting with profileachievementsteams
+ */
+class ProfileAchievementsTeamController {
+  /**
+   * Show a list of all profileachievementsteams.
+   * GET profileachievementsteams
+   */
+  async index ({ request, response, view }) {
+  }
+
+  /**
+   * Render a form to be used for creating a new profileachievementsteam.
+   * GET profileachievementsteams/create
+   */
+  async create ({ request, response, view }) {
+  }
+
+  /**
+   * Create/save a new profileachievementsteam.
+   * POST profileachievementsteams
+   */
+  async store ({ request, response }) {
+  }
+
+  /**
+   * Display a single profileachievementsteam.
+   * GET profileachievementsteams/:id
+   */
+  async show ({ params, request, response, view }) {
+  }
+
+  /**
+   * Render a form to update an existing profileachievementsteam.
+   * GET profileachievementsteams/:id/edit
+   */
+  async edit ({ params, request, response, view }) {
+  }
+
+  /**
+   * Update profileachievementsteam details.
+   * PUT or PATCH profileachievementsteams/:id
+   */
+  async update ({ params, request, response }) {
+  }
+
+  /**
+   * Delete a profileachievementsteam with id.
+   * DELETE profileachievementsteams/:id
+   */
+  async destroy ({ params, request, response }) {
+  }
+}
+
+module.exports = ProfileAchievementsTeamController
