74d5efb0dfca51cdd52ff1ff083e77f861809063
Leochrisis <leonardo.chfc@gmail.com>
2018-09-16 23:51:10 -0300

    Start standardize sessions methods names

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -1,7 +1,7 @@
 'use strict'
 
 class SessionController {
-  async store ({ request, auth }) {
+  async login ({ request, auth }) {
     const { email, password } = request.all()
 
     const token = await auth.attempt(email, password)
