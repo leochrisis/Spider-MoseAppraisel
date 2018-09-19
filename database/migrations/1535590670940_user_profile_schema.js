f4f51958d6cc7923e809270821e1b3fef6bbb848
leochrisis <leonardo.chfc@gmail.com>
2018-09-19 15:42:44 -0300

    Better visualization of user_profile schema

--- database/migrations/1535590670940_user_profile_schema.js
+++ database/migrations/1535590670940_user_profile_schema.js
@@ -6,20 +6,10 @@ class UserProfileSchema extends Schema {
   up () {
     this.create('user_profiles', (table) => {
       table.increments()
-      table
-        .integer('user_id')
-        .unsigned()
-        .references('id')
-        .inTable('users')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
-      table
-        .integer('profile_id')
-        .unsigned()
-        .references('id')
-        .inTable('profiles')
-        .onUpdate('CASCADE')
-        .onDelete('CASCADE')
+      table.integer('userId').unsigned()
+      table.foreign('userId').references('users.id')
+      table.integer('profileId').unsigned()
+      table.foreign('profileId').references('profiles.id')
       table.timestamps()
     })
   }
