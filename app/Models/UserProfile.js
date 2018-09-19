f4c3601d96fc90518ea9179763e1ef6c74808591
leochrisis <leonardo.chfc@gmail.com>
2018-09-19 15:40:06 -0300

    Remove unused relations from UserProfile model

--- app/Models/UserProfile.js
+++ app/Models/UserProfile.js
@@ -3,16 +3,6 @@
 const Model = use('Model')
 
 class UserProfile extends Model {
-  user () {
-    return this.hasMany('App/Models/User')
-  }
-
-  profile () {
-    return this.hasMany('App/Models/Profile')
-  }
-
-  achievement () {
-    return this.hasMany('App/Models/Achievement')
   }
 }
 
