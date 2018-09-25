a15d5bc87945ba8fd4172037ee4373f92218e488
Leochrisis <leonardo.chfc@gmail.com>
2018-09-25 00:03:22 -0300

    Setup sponsor unit CRUD

---
+++ resources/pages/sponsor/unit/_id.vue
@@ -0,0 +1,272 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-one-fifth">
+        <aside class="menu">
+          <div class="card">
+            <div class="card-image">
+              <figure class="image is-4by3">
+                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+              </figure>
+            </div>
+            <div class="card-content">
+              <div class="media">
+                <div class="media-left">
+                  <figure class="image is-48x48">
+                    <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                  </figure>
+                </div>
+                <div class="media-content">
+                  <p class="title is-4">John Smith</p>
+                  <p class="subtitle is-6">@johnsmith</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br/>
+
+          <ul class="menu-list gray">
+            <a class="button is-info" @click="creation = true">Novo empreendimento</a>
+            <br/>
+            <p class="menu-label">
+              Empreendimentos:
+            </p>
+            <ul>
+              <div v-for="achievement in achievements" :key="achievement.id">
+                <li><a @click="chargeAchievement(achievement.id)">{{achievement.name}}</a></li>
+                  <li>
+                    <ul>
+                      <div v-for="unit in achievement.unit" :key="unit.id">
+                        <li><a>
+                        <nuxt-link :to="`/sponsor/unit/${unit.id}`">
+                          {{unit.name}}
+                        </nuxt-link>
+                        </a></li>
+                      </div>
+                    </ul>
+                  </li>
+              </div>
+            </ul>
+          </ul>
+        </aside>
+      </div>
+
+      <div class="column" v-if="selected">
+        <nav class="navbar is-ligthGray">
+          <div class="navbar-start">
+            <div class="navbar-item title">
+              {{selected.name}}
+            </div>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <button class="button is-warning" @click="editUnit">Editar</button>
+                <button class="button is-danger" @click="deleteUnit">Deletar</button>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <nav class="level">
+          <div class="level-item has-text-centered column is-2">
+            <div>
+              <p class="heading">Responsável</p>
+              <p>
+                A unidade de negócio não possui responsável.
+                <a @click="responsible = true">Clique aqui para cadastra-lo</a>
+              </p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered column is-2">
+            <div>
+              <p class="heading">Descrição</p>
+              <p>{{selected.description}}</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered column is-2">
+            <div>
+              <p class="heading">Telefone</p>
+              <p>{{selected.phone}}</p>
+            </div>
+          </div>
+          <div class="level-item has-text-centered column is-2">
+            <div>
+              <p class="heading">Número de membros</p>
+              <p>{{selected.people_number}}</p>
+            </div>
+          </div>
+        </nav>
+        <nav class="navbar is-transparent">
+          <div class="navbar-start">
+            <div class="navbar-item title">
+              Avaliações
+            </div>
+          </div>
+        </nav>
+        <br/>
+        <div v-if="selected.evaluation.length === 0">
+          Ainda não existem avaliações.
+        </div>
+        <div v-else>
+          <b-table
+            :bordered="bordered"
+            :data="selected.evaluation"
+            :columns="columns"
+            focusable
+          ></b-table>
+        </div>
+      </div>
+    </div>
+    <section>
+      <div v-if="edition">
+        <b-modal :active.sync="edition" has-modal-card>
+          <form action="">
+            <div class="modal-card" style="width: auto">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Atualização de unidade de negócio</p>
+                </header>
+                <section class="modal-card-body">
+                  <b-field label="Nome">
+                    <b-input
+                        v-model="edited.name"
+                        placeholder="Nome da unidade"
+                        required>
+                    </b-input>
+                  </b-field>
+
+                  <b-field label="Descrição">
+                    <b-input
+                        type="textarea"
+                        v-model="edited.description"
+                        placeholder="Descrição da unidade"
+                        required>
+                    </b-input>
+                  </b-field>
+
+                  <b-field label="Telefone">
+                    <b-input
+                        v-model="edited.phone"
+                        placeholder="Telefone da unidade de negócio"
+                        required>
+                    </b-input>
+                  </b-field>
+
+                  <b-field label="Número de pessoas">
+                    <b-input
+                        v-model="edited.people_number"
+                        placeholder="Número de pessoas da unidade"
+                        required>
+                    </b-input>
+                  </b-field>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button" type="button" @click="edition = false">Cancelar</button>
+                    <button class="button is-primary" @click="updateUnit">Atualizar</button>
+                </footer>
+            </div>
+          </form>
+        </b-modal>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'basic',
+
+  async created () {
+    const achievements = await this.$axios.$get('/api/achievements/3')
+    this.achievements.push(achievements)
+    var id = this.$route.params.id
+    const selected = await this.$axios.$get(`/api/unit/${id}`)
+    this.selected = selected
+  },
+
+  data: () => ({
+    achievements: [],
+    selected: null,
+    responsible: false,
+    bordered: true,
+    creation: false,
+    edition: false,
+    evaluation: {
+      unit_id: '',
+      type: 'contetxo',
+      status: 'vigente',
+      contractor: '',
+      partner: ''
+    },
+    contractors: [
+      'Lorem',
+      'Inpsun',
+      'Sit amet'
+    ],
+    columns: [
+      {
+        field: 'type',
+        label: 'Tipo',
+        centered: true
+      },
+      {
+        field: 'status',
+        label: 'status',
+        centered: true
+      },
+      {
+        field: 'contractor',
+        label: 'Contratante',
+        centered: true
+      },
+      {
+        field: 'partner',
+        label: 'Parceiro',
+        centered: true
+      }
+    ],
+    edited: {}
+  }),
+
+  methods: {
+    async createEvaluation () {
+      this.evaluation.unit_id = this.$route.params.id
+      await this.$axios.$post(`api/evaluation`, this.evaluation)
+    },
+
+    editEvaluation () {
+      Object.assign(this.edited, this.selectedEva)
+      this.edition = true
+    },
+
+    async updateEvaluation () {
+      const {id} = this.edited
+      await this.$axios.$put(`api/evaluation/${id}`, this.edited)
+      Object.assign(this.selected, this.edited)
+    },
+
+    async deleteEvaluation () {
+      const {id} = this.selected
+      await this.$axios.$delete(`api/evaluation/${id}`)
+    },
+
+    editUnit () {
+      Object.assign(this.edited, this.selected)
+      this.edition = true
+    },
+
+    async updateUnit () {
+      const {id} = this.edited
+      await this.$axios.$put(`api/unit/${id}`, this.edited)
+      Object.assign(this.selected, this.edited)
+    },
+
+    async deleteUnit () {
+      const {id} = this.selected
+      await this.$axios.$delete(`api/unit/${id}`)
+      this.$router.push({path: '/sponsor', success: true})
+    }
+  }
+}
+</script>
+
