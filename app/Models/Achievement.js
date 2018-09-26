7588e6b865d61228a0178d6c84a643f15d8a8a2b
leochrisis <leonardo.chfc@gmail.com>
2018-09-26 16:29:20 -0300

    Remove unused userProfile function

--- app/Models/Achievement.js
+++ app/Models/Achievement.js
@@ -3,10 +3,6 @@
 const Model = use('Model')
 
 class Achievement extends Model {
-  userprofile () {
-    return this.belongsTo('App/Models/UserProfile')
-  }
-
   unit () {
     return this.hasMany('App/Models/Unit')
   }
