772a62aaf6a2001813653f4d6274a8efdb242e92
leochrisis <leonardo.chfc@gmail.com>
2018-09-27 13:34:43 -0300

    Remove unused container class

--- resources/layouts/auth.vue
+++ resources/layouts/auth.vue
@@ -3,29 +3,27 @@
     <section class="hero is-primary is-fullheight">
       <div class="hero-head">
         <nav class="navbar">
-          <div class="container">
-            <div class="navbar-brand">
-              <a class="navbar-item">
-                Mose competence
+          <div class="navbar-brand">
+            <a class="navbar-item">
+              Mose competence
+            </a>
+            <span class="navbar-burger burger" data-target="navbarMenuHeroA">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenuHeroA" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item" href="https://github.com/projeto-spider" target="blank">
+                Projeto spider
               </a>
-              <span class="navbar-burger burger" data-target="navbarMenuHeroA">
-                <span></span>
-                <span></span>
-                <span></span>
-              </span>
-            </div>
-            <div id="navbarMenuHeroA" class="navbar-menu">
-              <div class="navbar-end">
-                <a class="navbar-item" href="https://github.com/projeto-spider" target="blank">
-                  Projeto spider
-                </a>
-                <a class="navbar-item" href="https://github.com/leochrisis/bronze-mose/" target="blank">
-                  Github
-                </a>
-                <a class="navbar-item">
-                  Sobre
-                </a>
-              </div>
+              <a class="navbar-item" href="https://github.com/leochrisis/bronze-mose/" target="blank">
+                Github
+              </a>
+              <a class="navbar-item">
+                Sobre
+              </a>
             </div>
           </div>
         </nav>
