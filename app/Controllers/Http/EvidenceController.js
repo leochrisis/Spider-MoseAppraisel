35b9c672fba3bd24dbe9c91e10e80cab4380cee5
leochrisis <leonardo.chfc@gmail.com>
2018-09-27 10:49:31 -0300

    Remove unused create and edit functions

--- app/Controllers/Http/EvidenceController.js
+++ app/Controllers/Http/EvidenceController.js
@@ -4,15 +4,10 @@ class EvidenceController {
   async index ({ request, response, view }) {
   }
 
-  async create ({ request, response, view }) {
-  }
   async store ({ request, response }) {
   }
 
   async show ({ params, request, response, view }) {
-  }
-
-  async edit ({ params, request, response, view }) {
   }
 
   async update ({ params, request, response }) {
