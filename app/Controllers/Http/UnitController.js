98b721284ee1979c9a3a019ef4d5fb06d642bed6
leochrisis <leonardo.chfc@gmail.com>
2018-09-27 10:51:19 -0300

    Remove unused comments

--- app/Controllers/Http/UnitController.js
+++ app/Controllers/Http/UnitController.js
@@ -2,14 +2,7 @@
 
 const Unit = use('App/Models/Unit')
 
-/**
- * Resourceful controller for interacting with units
- */
 class UnitController {
-  /**
-   * Show a list of all units.
-   * GET units
-   */
   async index () {
     return await Unit
       .query()
@@ -17,17 +10,9 @@ class UnitController {
       .fetch()
   }
 
-  /**
-   * Render a form to be used for creating a new unit.
-   * GET units/create
-   */
   async create ({ request, response, view }) {
   }
 
-  /**
-   * Create/save a new unit.
-   * POST units
-   */
   async store ({ request, response }) {
     const data = request.only(['achievement_id', 'name', 'description', 'phone', 'people_number'])
 
@@ -36,28 +21,16 @@ class UnitController {
     return unit
   }
 
-  /**
-   * Display a single unit.
-   * GET units/:id
-   */
   async show ({ request }) {
     return await Unit.query()
       .where('id', request.params.id)
       .first()
   }
 
-  /**
-   * Render a form to update an existing unit.
-   * GET units/:id/edit
-   */
   async edit ({ params, request, response, view }) {
       .with('evaluations')
   }
 
-  /**
-   * Update unit details.
-   * PUT or PATCH units/:id
-   */
   async update ({ params, request, response }) {
     const unit = await Unit.findOrFail(request.params.id)
     const data = request.only(['name', 'description', 'phone', 'people_number'])
@@ -66,10 +39,6 @@ class UnitController {
     return unit
   }
 
-  /**
-   * Delete a unit with id.
-   * DELETE units/:id
-   */
   async destroy ({ params }) {
     const unit = await Unit.findOrFail(params.id)
     await unit.delete()
