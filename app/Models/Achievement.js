b0aeba2b0988855e11a106e255cdf206b6bdde3e
Leochrisis <leonardo.chfc@gmail.com>
2018-09-28 04:31:48 -0300

    Add roles function on achievement's model

--- app/Models/Achievement.js
+++ app/Models/Achievement.js
@@ -6,6 +6,12 @@ class Achievement extends Model {
   units () {
     return this.hasMany('App/Models/Unit', 'id', 'achievementId')
   }
+
+  roles () {
+    return this
+      .belongsToMany('App/Models/UserProfile', 'achievementId', 'userProfileId')
+      .pivotTable('profile_achievements_teams')
+  }
 }
 
 module.exports = Achievement
