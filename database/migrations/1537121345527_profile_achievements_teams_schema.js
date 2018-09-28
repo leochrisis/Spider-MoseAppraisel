b64bc0119c5b9438eecaba1db465130585a35d4f
Leochrisis <leonardo.chfc@gmail.com>
2018-09-28 04:21:12 -0300

    Define one foreing key on userProfile achievement relationship

--- database/migrations/1537121345527_profile_achievements_teams_schema.js
+++ database/migrations/1537121345527_profile_achievements_teams_schema.js
@@ -6,10 +6,8 @@ class ProfileAchievementsTeamsSchema extends Schema {
   up () {
     this.create('profile_achievements_teams', (table) => {
       table.increments()
-      table.integer('userId').unsigned()
-      table.foreign('userId').references('users.id')
-      table.integer('profileId').unsigned()
-      table.foreign('profileId').references('profiles.id')
+      table.integer('userProfileId').unsigned()
+      table.foreign('userProfileId').references('user_profiles.id')
       table.integer('achievementId').unsigned()
       table.foreign('achievementId').references('achievements.id')
       table.timestamps()
