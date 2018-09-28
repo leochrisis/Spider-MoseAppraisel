e47caa1c771a04290c4036ea4ffba85b7c6b7453
Leochrisis <leonardo.chfc@gmail.com>
2018-09-28 04:38:06 -0300

    Start responsible pages

---
+++ resources/pages/responsible/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    olá
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'basic',
+
+  data: () => ({
+    units: []
+  }),
+
+  async created () {
+    const units = await this.$axios.$get('/api/units')
+    this.units = units
+  },
+
+  methods: {}
+}
+</script>
