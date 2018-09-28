7bfa614e8e3ad0034a866d9d8ae254eaca3e1fb1
Leochrisis <leonardo.chfc@gmail.com>
2018-09-28 04:32:01 -0300

    Fix typo

--- app/Controllers/Http/EvaluationController.js
+++ app/Controllers/Http/EvaluationController.js
@@ -31,7 +31,7 @@ class EvaluationController {
    * POST evaluations
    */
   async store ({ request, response }) {
-    const data = request.only(['unit_id', 'type', 'status', 'contractor', 'partner'])
+    const data = request.only(['unitId', 'type', 'status', 'contractor', 'partner'])
 
     const evaluation = await Evaluation.create(data)
 
