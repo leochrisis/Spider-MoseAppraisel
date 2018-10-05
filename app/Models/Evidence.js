f66a44213e6c849e467fe9205d419a2c14424067
Leochrisis <leonardo.chfc@gmail.com>
2018-10-04 22:51:08 -0300

    Add owners function to evidence's model

--- app/Models/Evidence.js
+++ app/Models/Evidence.js
@@ -6,6 +6,12 @@ class Evidence extends Model {
   units () {
     return this.belongsTo('App/Models/Unit', 'unitId')
   }
+
+  owners () {
+    return this
+      .belongsToMany('App/Models/Team', 'evidenceId', 'memberId')
+      .pivotTable('member_roles')
+  }
 }
 
 module.exports = Evidence
