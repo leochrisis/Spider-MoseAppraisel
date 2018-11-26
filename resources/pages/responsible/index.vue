7a3e3d67c9e507ed2e984f89e5cafb22bdfe3c29
leochrisis <leonardo.chfc@gmail.com>
2018-11-26 11:29:41 -0300

    Add confidentiality term to application

--- resources/pages/responsible/index.vue
+++ resources/pages/responsible/index.vue
@@ -83,6 +83,7 @@
                           <nuxt-link :to="`/responsible/evidences/${selectedUn.id}`">
                             Evidências
                           </nuxt-link>
+                          <a class="button" @click="term = true">Acordo de confidencialidade</a>
                         </div>
                         <div v-else>
                           <button class="button" @click="">Plano de melhoria</button>
@@ -228,6 +229,53 @@
           </form>
         </b-modal>
       </div>
+      <div v-if="term">
+        <b-modal :active.sync="term" has-modal-card>
+          <form action="">
+            <div class="modal-card" style="width: auto">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Acordo de confidencialidade</p>
+                </header>
+                <section class="modal-card-body">
+                  <p>
+                    A  unidade de negócio {{selectedUnit.name}} está executando
+                    uma avaliação de contexto segundo o método de avaliação MOSE SM ®.
+                    Com o intuito dar maior liberdade aos entrevistados e de preservar o
+                    negócio do empreendimento avaliado será acordado:
+                  </p>
+                  <p>
+                    1. Todas as informações registradas durante a avaliação, tais como
+                    questionários, discussões, entrevistas serão tratados pelo líder da avaliação e
+                    pela equipe de avaliação (se houver) com confidencialidade e não serão
+                    reportados para outros que não pertençam a equipe de avaliação;
+                  </p>
+                  <p>
+                    2. Todos os participantes da avaliação (membros da equipe de avaliação,
+                    pessoas que forem entrevistadas e patrocinador) concordam de não discutir
+                    as informações compartilhadas com outras pessoas;
+                  </p>
+                  <p>
+                    3. Todos os resultados da avaliação (exemplo: Relatório de
+                    Inconsistências, Relatório de Achados, entre outros) serão documentados e
+                    apresentados sem atribuição a indivíduos específicos;
+                  </p>
+                  <p>
+                    4. Todos os resultados da avaliação são de propriedade do patrocinador
+                    e não pode ser compartilhados sem que seja autorizado por ele.
+                  </p>
+
+                    Nós abaixo assinados entendemos e concordamos com as cláusulas acima
+                    descritas.
+                  </p>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button" type="button" @click="term = false">Cancelar</button>
+                    <button class="button is-primary" @click="">Concordar</button>
+                </footer>
+            </div>
+          </form>
+        </b-modal>
+      </div>
     </section>
   </div>
 </template>
@@ -255,6 +303,7 @@ export default {
     selectedEv: false,
     editionEv: false,
     selectedME: false,
+    term: false,
     columns: [
       {
         field: 'id',
